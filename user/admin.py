c38a62bbb1b683edae8b498128e6c63f9cb98ad3
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-05-26 22:56:07 +0700

    update model user

--- user/admin.py
+++ user/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
-
 from .models import User
-from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 
-class UserInline(admin.TabularInline):
-    model = User
-    extra = 0
-
-@admin.register(User)
-class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ['*']
-    inlines = [UserInline]
-    readonly_fields = ['total_amount', 'created_at']
