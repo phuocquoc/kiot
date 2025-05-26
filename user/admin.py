633a7d1fdb23aae7ce34bcd97a8ba91711addffa
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-05-26 22:54:30 +0700

    add user to admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from django.contrib.auth.models import User
+from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(User, UserAdmin)
+
+class UserInline(admin.TabularInline):
+    model = User
+    extra = 0
+
+@admin.register(User)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ['*']
+    inlines = [UserInline]
+    readonly_fields = ['total_amount', 'created_at']
